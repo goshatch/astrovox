b680dcca48780d858086dc4b5d7db89a7420c91e
Gosha Tcherednitchenko <git@gosha.net>
2023-03-29 21:15:55 +0100

    change: Somewhat working waveform visualisation

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include <locale.h>
+#include <math.h>
 #include <ncurses.h>
 #include <signal.h>
 #include <stdio.h>
@@ -15,6 +16,10 @@ int main(void) {
 
   struct app_state state = { .gen_index = 0, .time_index = 0.0 };
 
+  double frequency = 20.0; // 20 Hz
+  double increment =
+      frequency / SAMPLE_RATE * (BUFFER_SIZE / (double)(WINDOW_WIDTH - 2));
+
   // Initialise ncurses UI
   setlocale(LC_ALL, "");
   initscr();
@@ -24,16 +29,8 @@ int main(void) {
 
   // Create window for displaying waveform
   WINDOW *win = newwin(WINDOW_HEIGHT, WINDOW_WIDTH, 0, 0);
-  box(win, 0, 0);
   wrefresh(win);
 
-  // NOTE:
-  // This is the main application loop. It should do the following:
-  // - Run the oscillator -- generate sample for every beat based on selected
-  //   waveform. This involves tracking time, so there should be some kind of
-  //   clock.
-  // - Update the UI to display the waveform from the generated sample
-  //   plus all previously generated ones.
   while (1) {
     clear();
 
@@ -41,18 +38,24 @@ int main(void) {
     double waveform[BUFFER_SIZE];
     for (int i = 0; i < BUFFER_SIZE; i++) {
       waveform[i] = generators[state.gen_index](state.time_index);
-      state.time_index += 1.0 / SAMPLE_RATE;
+      state.time_index += increment;
+    }
+
+    // Find the maximum absolute value of the waveform
+    double max_val = 0.0;
+    for (int i = 0; i < BUFFER_SIZE; i++) {
+      double abs_val = fabs(waveform[i]);
+      if (abs_val > max_val) {
+        max_val = abs_val;
+      }
     }
 
     // Display the waveform
-    plot_waveform(waveform, BUFFER_SIZE, 1.0, win);
-
+    plot_waveform(waveform, BUFFER_SIZE, max_val, win);
     mvprintw(WINDOW_HEIGHT + 1, 0, "OSC %s | TIME %f", wave_name(state.gen_index), state.time_index);
-
     refresh();
 
     int c = getch();
-
     if (c == 'q' || c == 'Q') {
       break;
     } else if (c == 'j') {
@@ -76,30 +79,46 @@ void sigint_handler(int sig) {
 void plot_waveform(double *waveform, int waveform_len, double max_val, WINDOW *win) {
   // Clear waveform window
   werase(win);
-  box(win, 0, 0);
 
   // Plot waveform
   double y_scale = (double)WINDOW_HEIGHT / (2.0 * max_val);
   double x_scale = (double)WINDOW_WIDTH / waveform_len;
+
   for (int i = 0; i < waveform_len - 1; i++) {
-    int x1 = i * x_scale;
-    int x2 = (i + 1) * x_scale;
-    int y1 = WINDOW_HEIGHT / 2 - (int)(waveform[i] * y_scale);
-    int y2 = WINDOW_HEIGHT / 2 - (int)(waveform[i + 1] * y_scale);
-    mvwaddch(win, y1, x1, ACS_DIAMOND); // Plot point on waveform
-    if (x1 == x2) {
-      continue; // Don't try to draw vertical lines
-    }
-    double m = ((double)(y2 - y1)) / ((double)(x2 - x1)); // Slope of line
-    double b = y1 - m * x1; // Y intercept of line
-    int step = (x2 - x1) > 0 ? 1 : -1;
-    for (int x = x1; x != x2; x += step) {
-      int y = (int)(m * x + b);
-      if (y >= 0 && y < WINDOW_HEIGHT) {
-        mvwaddch(win, y, x, ACS_DIAMOND); // Plot point on line
+    int x1 = i * x_scale + 1;
+    int x2 = (i + 1) * x_scale + 1;
+    int y1 = (WINDOW_HEIGHT - 2) / 2 - (int)(waveform[i] * y_scale) + 1;
+    int y2 = (WINDOW_HEIGHT - 2) / 2 - (int)(waveform[i + 1] * y_scale) + 1;
+
+    // Draw a line between the two points
+    int y_diff = abs(y2 - y1);
+    int x_diff = abs(x2 - x1);
+    int y_sign = y1 < y2 ? 1 : -1;
+    int y = y1;
+
+    if (x_diff >= y_diff) {
+      int err = x_diff / 2;
+      for (int x = x1; x <= x2; x++) {
+        mvwaddch(win, y, x, '*');
+        err -= y_diff;
+        if (err < 0) {
+          y += y_sign;
+          err += x_diff;
+        }
+      }
+    } else {
+      int err = y_diff / 2;
+      for (int y = y1; y != y2 + y_sign; y += y_sign) {
+        mvwaddch(win, y, x1, '*');
+        err -= x_diff;
+        if (err < 0) {
+          x1++;
+          err += y_diff;
+        }
       }
     }
   }
 
-  wrefresh(win);
+  wnoutrefresh(win);
+  doupdate();
 }
